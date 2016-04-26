be5dbd6fa2e2a7d4bc886fc0b5ce70ff3c893474
GaretWebster <gdubrocks@gmail.com>
2016-04-25 22:17:20 -0700

    If your name isn't owner you can't see catagories or products

---
+++ WebContent/js/main.js
@@ -0,0 +1,20 @@
+var userName = 'Owner';
+
+function toggleOwner() { 
+
+	var userNameElement=document.getElementById("userName");
+	userName=userNameElement.innerHTML;
+	
+    var mainElement = document.getElementById('main');
+	var errorElement = document.getElementById('error');
+				
+    //if (session.getAttribute("is_owner") == false) {
+	if(userName != 'Owner'){
+		mainElement.style.display = 'none';
+		errorElement.style.display = 'block';
+    }
+    else { 
+		mainElement.style.display = 'block';
+		errorElement.style.display = 'none';
+    }
+}  
