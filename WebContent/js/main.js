31c85aaf618480b13354e403adf7cba1aa06e146
GaretWebster <gdubrocks@gmail.com>
2016-05-01 18:43:11 -0700

    made main not kick people off

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -2,20 +2,22 @@ var userName = 'Owner';
 
 function toggleOwner() { 
 
-	var userNameElement=document.getElementById("userName");
-	userName=userNameElement.innerHTML;
+	//var userNameElement=document.getElementById("userName");
+	//userName=userNameElement.innerHTML;
 	
     var mainElement = document.getElementById('main');
 	var errorElement = document.getElementById('error');
 				
     //if (session.getAttribute("is_owner") == false) {
-	//if(userName != 'Owner'){
 	if(userName == 'Owner'){
-		mainElement.style.display = 'none';
-		errorElement.style.display = 'block';
-    }
-    else { 
+		//display main, hide error
 		mainElement.style.display = 'block';
 		errorElement.style.display = 'none';
+
+    }
+    else { 
+		//display error, hide main
+		mainElement.style.display = 'none';
+		errorElement.style.display = 'block';
     }
 }  
